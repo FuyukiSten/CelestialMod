3ed12268491e866a42ef9b1e8aa038aa2881ea8b
Kirizuna <askibanz@gmail.com>
2018-11-04 15:57:37 -0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
 require('./util/eventLoader')(client);
-
+//
 const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
