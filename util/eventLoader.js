f7427c433908f76c89f25d7d15649246bde19eea
Kirizuna <askibanz@gmail.com>
2018-11-04 15:32:48 -0200

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,5 +4,5 @@ module.exports = client => {
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
   client.on('message', reqEvent('message'));
-  client.on('guildMemberAdd.js', reqEvent('guildMemberAdd')(client)):
+  client.on('guildMemberAdd', () => reqEvent('guildMemberAdd')(member)):
 };
