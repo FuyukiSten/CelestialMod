931117edb21ec24cd3fc05897dc0133a6618a414
fruuwuuio <45402727+fruuwuuio@users.noreply.github.com>
2018-11-27 22:02:05 -0200

    Create roleinfo.js

---
+++ commands/roleinfo.js
@@ -0,0 +1,318 @@
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => {
+    let cargo = args[0];
+    if(!cargo) {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.setDescription(`${message.author}, providencie o parâmetro necessário, exemplo: c!roleinfo Admin`);
+   message.channel.send(embed);
+
+        return;
+    }
+
+    if(cargo == "Banner") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, '**Cargo com á função de Banir membros cujo á grávidade da quebra das regras do clã, ou traição sejam muito sérias. Banner é o segundo maior Cargo para se tornar um Líder Primordial Dragons.**') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "Conselheiro") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, '**Conselheiro Primordial é responsável por dar conselhos tanto para os Líderes do clã como para os membros normais, com o objetivo de dar uma opinião sobre algo que deve ou não deve ser feito.**') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "General") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, '**General possuí como objetivo comandar o clã inteiro e dar medalhas após guerras de longa duração: 1 hora e 30 minutos. O mesmo também poderá ter um límite de esquadra de 30 membros.**') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+if(cargo == "") // Nome do cargo
+    {
+        let embed = new Discord.RichEmbed();
+        embed.setColor('RANDOM');
+        embed.addField(cargo, 'descrição') // Altere somente a descrição
+   message.channel.send(embed);
+        return;
+    }
+
+    let embed = new Discord.RichEmbed();
+    embed.setColor('RANDOM');
+    embed.setDescription(`${message.author}, não sou capaz de encontrar o cargo com os parâmetros informados.`);
+   message.channel.send(embed);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'roleinfo',
+  description: 'Veja a descrição de um cargo.',
+  usage: 'roleinfo <nome do cargo>'
+};
